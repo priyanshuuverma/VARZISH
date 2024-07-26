87c9bc9d0b81b22fbccfc9e40a42d8a89861da9f
priyanshuuverma <officialpriyanshu2003@gmail.com>
2024-07-26 08:01:23 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,37 +1,28 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import correct components
 import Navbar from './Components/Navbar';
 import Header from './Components/Header/Header';
 import Feature from './Components/Feature';
 import Offer from './Components/Offer';
 import About from './Components/About';
 import Contact from './Components/Contact';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      
-      <Navbar/>
-      <switch>
-      <Router path="/main">
-      <Header/>
-      </Router>
-      <Router path="/features">
-      <Feature/>
-      </Router>
-      <Router path="/presentation">
-      <Offer/>
-      </Router><Router exact path="/about">
-      <About/>
-      </Router>
-      <Router path="/contact">
-      <Contact/>
-      </Router>
-      </switch>
-      
-      
-      
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/main" element={<Header />} />
+          <Route path="/features" element={<Feature />} />
+          <Route path="/presentation" element={<Offer />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          {/* You can add a fallback route for unmatched paths */}
+          <Route path="*" element={<Header />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
