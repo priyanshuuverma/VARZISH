6172760825c2613ad4db8b2d1c04c8d9449485b7
priyanshuuverma <officialpriyanshu2003@gmail.com>
2024-07-26 09:17:50 +0530

    Update Checkbox.jsx

--- src/Components/Checkbox/Checkbox.jsx
+++ src/Components/Checkbox/Checkbox.jsx
@@ -13,16 +13,6 @@ function Checkbox()
       <path d="M2 8.36364L6.23077 12L13 2"></path>
     </svg>
   </div>
-  
-  <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
-    <defs>
-      <filter id="goo-12">
-        <fegaussianblur in="SourceGraphic" stddeviation="4" result="blur"></fegaussianblur>
-        <fecolormatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 22 -7" result="goo-12"></fecolormatrix>
-        <feblend in="SourceGraphic" in2="goo-12"></feblend>
-      </filter>
-    </defs>
-  </svg>
 </div>
         </>
     )
