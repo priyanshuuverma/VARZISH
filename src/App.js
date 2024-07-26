3ec3b5a4897d18ecb51a7fe80e0c14abba1e0a69
priyanshuuverma <officialpriyanshu2003@gmail.com>
2024-07-26 08:20:04 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import correct components
 import Navbar from './Components/Navbar';
 import Header from './Components/Header/Header';
 import Feature from './Components/Feature';
@@ -9,20 +8,14 @@ import Contact from './Components/Contact';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <Routes>
-          <Route path="/main" element={<Header />} />
-          <Route path="/features" element={<Feature />} />
-          <Route path="/presentation" element={<Offer />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          {/* You can add a fallback route for unmatched paths */}
-          <Route path="*" element={<Header />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <Navbar />
+      <Header id="main" />
+      <Feature id="features" />
+      <Offer id="presentation" />
+      <About id="about" />
+      <Contact id="contact" />
+    </div>
   );
 }
 
